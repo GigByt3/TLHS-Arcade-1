94dff2221f93c4bdf26eb47cd66a751eb3ad327e
cvr-software <cvrgaming@gmail.com>
2020-01-23 21:29:02 -0800

    made temp movement script
    i know im not supposed to, but i need a way to move around the level okay

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    private Rigidbody rb;
+    public float speed;
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        checkKeys();
+    }
+
+    void checkKeys()
+    {
         if (Input.GetKey("up"))
         {
+            transform.Translate(Vector3.forward * speed);
+        }
 
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.back * speed);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -5, 0, Space.Self);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 5, 0, Space.Self);
         }
     }
 }
