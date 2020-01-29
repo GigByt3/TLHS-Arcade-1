bf57598fb7ab008706621ccaebcd6ff602085609
LiteVoid <56370802+LiteVoid@users.noreply.github.com>
2020-01-29 15:12:43 -0800

    interface

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface : MonoBehaviour
+{
+    // Still needs to have fields
+
+    // This is the interface method
+
+    public interface IInteractable
+    {
+        void interact();
+    }
+
+}
