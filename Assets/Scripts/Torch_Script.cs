36ac39ccc9216c3e799e6a86aa07eb1fbe40cc18
LiteVoid <56370802+LiteVoid@users.noreply.github.com>
2020-03-11 15:57:23 -0700

    Torch_Script has been made to turn torches when the player moves.
    This will make the torches rotate between two different angles when the player turns.

---
+++ Assets/Scripts/Torch_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch_Script : MonoBehaviour
+{
+    void OnEnable()
+    {
+        PlayerMovement.onTurn += TurnTorch;
+    }
+
+
+    void OnDisable()
+    {
+        PlayerMovement.onTurn -= TurnTorch;
+    }
+
+    void TurnTorch()
+    {
+
+        //this.transform.Rotate(90, 0, 0); //turns 90
+
+
+        if (hasTurned)
+        {
+            this.transform.Rotate(90, 0, 0);
+            hasTurned = false;
+        }
+        else
+        {
+            this.transform.Rotate(-90, 0, 0);
+        }
+    }
+}
+
