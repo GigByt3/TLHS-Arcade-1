0fab18bc94fbaaa637ed924b023d105afa39bd5a
cvr-software <cvrgaming@gmail.com>
2020-03-11 16:16:36 -0700

    wokring on amination things

---
+++ Assets/Scripts/ApplyTexture.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyTexture : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
