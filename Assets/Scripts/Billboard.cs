8de04cf2c400d5c03da7cc34c57ebbb9bfd0c8af
cvr-software <cvrgaming@gmail.com>
2020-03-11 16:59:41 -0700

    billboarding

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        Quaternion playerRot = player.transform.rotation;
+        this.transform.rotation = playerRot;
+    }
+}
