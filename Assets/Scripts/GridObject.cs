a7b0dfe3a72627ee1a165b246ecc1048e2a30a6f
cvr-software <cvrgaming@gmail.com>
2020-09-23 17:34:48 -0700

    Set up framework for LevelController, Maze, and GridObject
    Most implementations to be done later.

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GridObject : MonoBehaviour
+{
+    private Vector3 gridCoords;
+
+    private Maze maze;
+
+    public GridObject(int x, int y, int facing)
+    {
+        //define maze here
+
+        //clamp x & y here
+
+        facing %= 4;
+
+        gridCoords = new Vector3(x, y, facing);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void move()
+    {
+        
+    }
+
+    void rotate(string direction)
+    {
+        switch (direction)
+        {
+            case "left":
+                gridCoords.z--;
+                break;
+            case "right":
+                gridCoords.z++;
+                break;
+        }
+
+        gridCoords.z %= 4;
+    }
+
+    void faceDirection(string direction)
+    {
+        switch (direction)
+        {
+            case "north":
+                gridCoords.z = 0;
+                break;
+            case "east":
+                gridCoords.z = 1;
+                break;
+            case "south":
+                gridCoords.z = 2;
+                break;
+            case "west":
+                gridCoords.z = 3;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    private int width, height;
+
+    private bool[,,] walls;
+
+    private Dictionary<Vector3, GridObject> gridObjectDict;
+
+    public Maze(int mWidth, int mHeight)
+    {
+        width = mWidth;
+        height = mHeight;
+
+        walls = new bool[width + 1, height + 1, 2];
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        populateMazeArray();
+
+        generateMazeMesh();
+
+        populateGridObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void populateMazeArray()
+    {
+
+    }
+
+    void populateGridObjects()
+    {
+
+    }
+
+    void generateMazeMesh()
+    {
+
+    }
+
+    void teleportObject(GridObject objectToMove, int x, int y)
+    {
+        //update hash table
+    }
+
+    void moveObject(GridObject objectToMove, int direction, int distance)
+    {
+        int tilesMoved = 0;
+        while (tilesMoved < distance)
+        {
+            //check if wall in front
+                //if not, continue
+                //if yes, canMove = false
+        }
+    }
+
+    public bool getWallFromCoords(int x, int y, int side)
+    {
+        return walls[x, y, side];
+    }
+
+    public bool getWallFromDirection(int x, int y, int side)
+    {
+        if (side == 2)
+        {
+            return walls[x, y + 1, 0];
+        } else if (side == 3)
+        {
+            return walls[x - 1, y, 1];
+        } else
+        {
+            return walls[x, y, side];
+        }
+    }
+}
