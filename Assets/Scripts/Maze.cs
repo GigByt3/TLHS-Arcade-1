3f0611c5f7350512a970714e01af240160340a09
cvr-software <cvrgaming@gmail.com>
2020-09-24 22:55:38 -0700

    Completed Maze Generation Algorithm
    How maze is rendered will be completely rewritten, but the actual maze data structure generation is working. Might be rewritten in the future for different style of maze, but that's for another time.

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -44,6 +44,7 @@ public class Maze : MonoBehaviour
 
     void populateMazeArray()
     {
+        //set all walls true
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -51,6 +52,95 @@ public class Maze : MonoBehaviour
                 walls[i, j, 0] = true;
                 walls[i, j, 1] = true;
             }
+        }
+
+        //define stack of previous cells
+        Stack<Vector2> previousCellStack = new Stack<Vector2>();
+        Stack<Vector2> visitedCellStack = new Stack<Vector2>();
+
+        int currentCellX = UnityEngine.Random.Range(1, width - 2);
+        int currentCellY = UnityEngine.Random.Range(1, height - 2);
+
+        //previousCellStack.Push(new Vector2(currentCellX, currentCellY));
+        //visitedCellStack.Push(new Vector2(currentCellX, currentCellY));
+
+        Debug.Log("Starting at Cell " + currentCellX + ", " + currentCellY);
+        Debug.Log("walls has " + walls.Length);
+
+
+        while (visitedCellStack.Count < width * height)
+        {
+            int faceToBreak = chooseAdjacentWallToBreak(currentCellX, currentCellY, visitedCellStack);
+
+            Debug.Log("Trying to break face " + faceToBreak + " from Cell " + currentCellX + ", " + currentCellY);
+
+            if (faceToBreak == -1)
+            {
+                //go back
+                Debug.Log("No face found to break from Cell " + currentCellX + ", " + currentCellY);
+
+                //add cell to list of visited cells if it isnt already
+                if (!visitedCellStack.Contains(new Vector2(currentCellX, currentCellY))) visitedCellStack.Push(new Vector2(currentCellX, currentCellY));
+
+                currentCellX = (int)previousCellStack.Peek().x;
+                currentCellY = (int)previousCellStack.Pop().y;
+
+                Debug.Log("Going back to Cell " + currentCellX + ", " + currentCellY);
+            }
+            else
+            {
+                Debug.Log("Face " + faceToBreak + " broken successfully!");
+
+                //break wall
+                setWallFromDirection(currentCellX, currentCellY, faceToBreak, false);
+
+                //add cell to list of previous cells
+                previousCellStack.Push(new Vector2(currentCellX, currentCellY));
+
+                //add cell to list of visited cells if it isnt already
+                if (!visitedCellStack.Contains(new Vector2(currentCellX, currentCellY))) visitedCellStack.Push(new Vector2(currentCellX, currentCellY));
+
+                //move through
+
+                int oldCellX = currentCellX;
+                int oldCellY = currentCellY;
+
+                currentCellX = (int)getNewThroughWallCoords(oldCellX, oldCellY, faceToBreak).x;
+                currentCellY = (int)getNewThroughWallCoords(oldCellX, oldCellY, faceToBreak).y;
+
+                Debug.Log("Moved from currentCell into Cell " + currentCellX + ", " + currentCellY);
+            }
+
+            Debug.Log("previousCellStack has " + previousCellStack.Count);
+            Debug.Log("visitedCellStack has " + visitedCellStack.Count);
+        } 
+    }
+
+    int chooseAdjacentWallToBreak(int x, int y, Stack<Vector2> visitedCellStack)
+    {
+        ArrayList availableWalls = new ArrayList();
+
+        if (y > 0 && getWallFromDirection(x, y, 0) && !visitedCellStack.Contains(getNewThroughWallCoords(x, y, 0))) availableWalls.Add("NORTH");
+        if (x < width - 1 && getWallFromDirection(x, y, 1) && !visitedCellStack.Contains(getNewThroughWallCoords(x, y, 1))) availableWalls.Add("EAST");
+        if (y < height - 1 && getWallFromDirection(x, y, 2) && !visitedCellStack.Contains(getNewThroughWallCoords(x, y, 2))) availableWalls.Add("SOUTH");
+        if (x > 0 && getWallFromDirection(x, y, 3) && !visitedCellStack.Contains(getNewThroughWallCoords(x, y, 3))) availableWalls.Add("WEST");
+
+        if (availableWalls.Count <= 0) return -1;
+
+        String pickedWall = (String) availableWalls[UnityEngine.Random.Range(0, availableWalls.Count - 1)];
+
+        switch (pickedWall)
+        {
+            case "NORTH":
+                return 0;
+            case "EAST":
+                return 1;
+            case "SOUTH":
+                return 2;
+            case "WEST":
+                return 3;
+            default:
+                return -1;
         }
     }
 
@@ -143,17 +233,96 @@ public class Maze : MonoBehaviour
         return walls[x, y, side];
     }
 
+    public Vector2 getNewThroughWallCoords(int x, int y, int side)
+    {
+        switch (side)
+        {
+            case 0:
+                return new Vector2(x, y - 1.0f);
+            case 1:
+                return new Vector2(x + 1.0f, y);
+            case 2:
+                return new Vector2(x, y + 1.0f);
+            case 3:
+                return new Vector2(x - 1.0f, y);
+            default:
+                return new Vector2(x, y);
+        }
+    }
+
+    public void setWallFromDirection(int x, int y, int side, bool newValue)
+    {
+        switch (side)
+        {
+            case 0:
+                if (y > 0)
+                {
+                    walls[x, y, side] = newValue;
+                }
+                break;
+            case 1:
+                if (x < width - 1)
+                {
+                    walls[x, y, side] = newValue;
+                }
+                break;
+            case 2:
+                if (y < height  -1)
+                {
+                    walls[x, y + 1, 0] = newValue;
+                }
+                break;
+            case 3:
+                if (x > 0)
+                {
+                    walls[x - 1, y, 1] = newValue;
+                }
+                break;
+        }
+    }
+
     public bool getWallFromDirection(int x, int y, int side)
     {
-        if (side == 2)
+        switch (side)
         {
-            return walls[x, y + 1, 0];
-        } else if (side == 3)
-        {
-            return walls[x - 1, y, 1];
-        } else
-        {
-            return walls[x, y, side];
+            case 0:
+                if (y <= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return walls[x, y, side];
+                }
+            case 1:
+                if (x >= width)
+                {
+                    return true;
+                }
+                else
+                {
+                    return walls[x, y, side];
+                }
+            case 2:
+                if (y >= height)
+                {
+                    return true;
+                }
+                else
+                {
+                    return walls[x, y + 1, 0];
+                }
+            case 3:
+                if (x <= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return walls[x - 1, y, 1];
+                }
+            default:
+                return true;
         }
     }
 }
