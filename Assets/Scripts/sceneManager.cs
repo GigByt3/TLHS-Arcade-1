994cfa0feb314199aa2badc7746638f63d598bd1
Finlay Norton-Lindsay <p3n.duke@gmail.com>
2020-10-07 23:43:05 -0700

    scene manager re-introduction

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+
+    static float position = 0;
+    /*
+     * SCENE DIRECTORY
+     * 0 - Main Menu
+     * 0.5 - First Transition
+     * 1 - First Level
+     * 1.5 Second Transition
+     * ...
+     * x - End Sceen
+     * 402 - Death Sceen
+     */
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        switch (position)
+        {
+            case 1;
+                break;
+            case 402:
+                //call a method.
+                break;
+            default:
+                //generate a maze ig.
+                break;
+        }
+    }
+
+    public void NextScene()
+    {
+        position += 0.5f;
+        if (Mathf.Floor(position) != position)
+        {
+            SceneManager.LoadScene("Transition_Area");
+        }
+        else
+        {
+            SceneManager.LoadScene("Game_Scene");
+        }
+    }
+
+    public void Death()
+    {
+        position = 402;
+        SceneManager.LoadScene("Transition_Area");
+    }
+
+    //Tools
+
+    public void GenerateMaze()
+    {
+
+    }
+}
--- Assets/Scripts/soundmanager.cs
+++ Assets/Scripts/soundmanager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class soundmanager : MonoBehaviour
+public class soundManager : MonoBehaviour
 {
 
     public AudioClip[] sounds;
