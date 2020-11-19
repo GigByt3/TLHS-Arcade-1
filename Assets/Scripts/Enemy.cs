64c40254800c8139a61dd9046bec0c4b62b76815
cvr-software <cvrgaming@gmail.com>
2020-11-18 16:49:05 -0800

    Added Enemy base script & GridObject Vision
    GridObject vision may be changed later

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : GridObject
+{
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        Player.moveEvent += AI;
+    }
+
+    void OnDisable()
+    {
+        Player.moveEvent -= AI;
+    }
+
+    public void AI()
+    {
+        if (player.isInCell(visibleNorthCells()))
+        {
+            faceDirection("north");
+            move(1);
+        }
+        else if (player.isInCell(visibleEastCells()))
+        {
+            faceDirection("east");
+            move(1);
+        } 
+        else if (player.isInCell(visibleSouthCells()))
+        {
+            faceDirection("south");
+            move(1);
+        }
+        else if (player.isInCell(visibleWestCells()))
+        {
+            faceDirection("west");
+            move(1);
+        }
+        else
+        {
+            switch (UnityEngine.Random.Range(0, 4))
+            {
+                case 0:
+                    faceDirection("north");
+                    break;
+                case 1:
+                    faceDirection("east");
+                    break;
+                case 2:
+                    faceDirection("south");
+                    break;
+                case 3:
+                    faceDirection("west");
+                    break;
+            }
+            move(1);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : GridObject
 {
-
+    public delegate void MoveDelegate();
+    public static event MoveDelegate moveEvent;
+    
     void Update()
     {
         checkKeys();
@@ -12,11 +15,28 @@ public class Player : GridObject
 
     void checkKeys()
     {
-        if (Input.GetKeyDown("up")) move(1);
-        if (Input.GetKeyDown("left")) rotate("left");
-        if (Input.GetKeyDown("right")) rotate("right");
+        if (Input.GetKeyDown("up"))
+        {
+            move(1);
+            moveEvent?.Invoke();
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            rotate("left");
+            moveEvent?.Invoke();
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            rotate("right");
+            moveEvent?.Invoke();
+        }
 
         //The following is a TESTING SCRIPT
         if (Input.GetKeyDown("n")) GameObject.FindGameObjectsWithTag("GameManager")[0].GetComponent<GameSceneManager>().NextScene();
+    }
+
+    public bool isInCell(List<Vector2Int> cells)
+    {
+        return cells.Contains(new Vector2Int(gridCoords.x, gridCoords.y));
     }
 }
