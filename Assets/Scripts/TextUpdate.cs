c993b5ae7e7ecad248a209865129ef50cfbf7b5b
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2021-05-13 18:22:55 -0700

    Transition Screen & Quotes Done
    -Imported Knight spritesheet, typewriter font, TextMeshPro
    -Animated spritesheet, adjusted background, text color, & UI
    -Built & Ran it, it works

---
+++ Assets/Scripts/TextUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextUpdate : MonoBehaviour
+{
+    public string[] quotes;
+    public TextMeshProUGUI displayText;
+    private int randomIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomIndex = Random.Range(0, quotes.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        
+        Debug.Log(randomIndex);
+        displayText.text = quotes[randomIndex];
+    }
+}
