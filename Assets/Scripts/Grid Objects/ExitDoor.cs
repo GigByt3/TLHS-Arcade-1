27e610b4ced06f0cd07f6e7439b25ff1b5019895
Emerson Coskey <emersoncoskey@gmail.com>
2021-06-01 21:03:33 -0700

    intentory thingsasdfasdf :)
    - added chest prefab WIP
    - added Interactible interface
    - moved exitDoor functionality to onInteract();

--- Assets/Scripts/Grid Objects/ExitDoor.cs
+++ Assets/Scripts/Grid Objects/ExitDoor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitDoor : GridObject
+public class ExitDoor : GridObject, Interactible
 {
     private float width, height;
 
@@ -20,5 +20,10 @@ public class ExitDoor : GridObject
         doorWall = transform.GetChild(0).gameObject;
         doorWall.transform.localScale = new Vector3(width / 10, height / 10, width / 10);
         doorWall.transform.localPosition = new Vector3(0, 0, width / 2);
+    }
+
+    public void onInteract()
+    {
+        GameObject.FindGameObjectsWithTag("GameManager")[0].GetComponent<GameSceneManager>().NextScene();
     }
 }
---
+++ Assets/Scripts/Grid Objects/Interactible.cs
@@ -0,0 +1,4 @@
+﻿public interface Interactible
+{
+    void onInteract();
+}
